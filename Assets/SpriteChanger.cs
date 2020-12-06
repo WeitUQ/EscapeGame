ccd877d0fc655f1ae2faf997a9ef5fbe5d47db95
WeitUQ <74394582+WeitUQ@users.noreply.github.com>
2020-12-07 01:57:41 +0900

    アイテム選択時の枠の挙動
    アイテム選択時、ほかのアイテムを選択するまではアイテム枠が赤のままになるようにした。

---
+++ Assets/SpriteChanger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteChanger : MonoBehaviour
+{
+    public Sprite[] sprite;
+    public GameObject[] itemList;
+    private GameObject iController;
+    private ItemController script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.iController = GameObject.Find("ItemController");
+        this.script = iController.GetComponent<ItemController>();
+        this.itemList = GameObject.FindGameObjectsWithTag("ItemList");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SpriteChange()
+    {
+        if(this.script.chooseHammer)
+        {
+            this.itemList[3].GetComponent<Image>().sprite = this.sprite[1];
+            this.itemList[4].GetComponent<Image>().sprite = this.sprite[0];
+            this.itemList[5].GetComponent<Image>().sprite = this.sprite[0];
+        }
+        else if (this.script.chooseNob)
+        {
+            this.itemList[3].GetComponent<Image>().sprite = this.sprite[0];
+            this.itemList[4].GetComponent<Image>().sprite = this.sprite[1];
+            this.itemList[5].GetComponent<Image>().sprite = this.sprite[0];
+        }
+        else if (this.script.chooseKey)
+        {
+            this.itemList[3].GetComponent<Image>().sprite = this.sprite[0];
+            this.itemList[4].GetComponent<Image>().sprite = this.sprite[0];
+            this.itemList[5].GetComponent<Image>().sprite = this.sprite[1];
+        }
+    }
+}
