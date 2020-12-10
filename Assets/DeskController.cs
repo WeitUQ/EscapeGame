ced458889e3ae9508b7544a5ea8a3e775cb00319
WeitUQ <74394582+WeitUQ@users.noreply.github.com>
2020-12-10 10:19:59 +0900

    ドアノブと引き出し
    ドアノブの一部変更と引き出しの開閉システムを追加した。

---
+++ Assets/DeskController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeskController : MonoBehaviour
+{
+    private Animator deskAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.deskAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DrawerStateChange1()
+    {
+            this.deskAnimator.SetBool("Open1", !this.deskAnimator.GetBool("Open1"));        
+    }
+    public void DrawerStateChange2()
+    {
+        this.deskAnimator.SetBool("Open2", !this.deskAnimator.GetBool("Open2"));
+    }
+    public void DrawerStateChange3()
+    {
+        this.deskAnimator.SetBool("Open3", !this.deskAnimator.GetBool("Open3"));
+    }
+}
