f3b3002d2e34b3d2e689816a55ed9b0bd9f00c95
WeitUQ <74394582+WeitUQ@users.noreply.github.com>
2020-12-15 12:39:41 +0900

    スロット(青)のリールを追加
    回転のアニメーションを導入した。

--- Assets/SlotMachineController.cs
+++ Assets/SlotMachineController.cs
@@ -7,14 +7,15 @@ public class SlotMachineController : MonoBehaviour
     public GameObject missPaper;
     public GameObject drawerKey;
     public GameObject[] items;
-    public GameObject redFrame;
+    public GameObject[] frames;
     public ItemController iScript;
     public Animator[] leverAnimator;
+    public Animator[] reelAnimator;
     private int prob;
     // Start is called before the first frame update
     void Start()
     {
-        
+ 
     }
 
     // Update is called once per frame
@@ -25,16 +26,17 @@ public class SlotMachineController : MonoBehaviour
     public void LeverONBlue()
     {
         this.leverAnimator[0].SetTrigger("ONBlue");
-        this.leverAnimator[0].SetBool("End", true);
+        if (this.iScript.bCoinCount == 3 )
+        {
+            this.reelAnimator[0].SetBool("Rotation", true);        }
     }
     public void LeverONRed()
     {
         this.leverAnimator[1].SetTrigger("ONRed");
-        this.leverAnimator[1].SetBool("End", true);
     }
     public void RedMachineController()
     {
-        if (this.iScript.rCoinCount == 3)
+        if (this.iScript.rCoinCount == 3 )
         {
             this.iScript.rCoinCount = 0;
             this.prob = Random.Range(0, 100);
@@ -45,22 +47,6 @@ public class SlotMachineController : MonoBehaviour
             else
             {
                 this.missPaper.SetActive(true);
-            }
-        }
-    }
-    public void BlueMachineController()
-    {
-        if (this.iScript.bCoinCount == 3)
-        {
-            this.iScript.bCoinCount = 0;
-            this.prob = Random.Range(0, 100);
-            if (this.prob >= 0 & this.prob <= 49)
-            {
-                this.items[0].SetActive(true);
-            }
-            else
-            {
-                this.items[1].SetActive(true);
             }
         }
     }
