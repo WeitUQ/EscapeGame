727ae01597f5cfdbe98aba135f012b334a148847
WeitUQ <74394582+WeitUQ@users.noreply.github.com>
2021-01-08 21:13:27 +0900

    PCログイン画面ほぼ完成

--- Assets/PCController.cs
+++ Assets/PCController.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PCController : MonoBehaviour
 {
+    public InputField inputField;
+    public Text underText;
+    public GameObject okButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,22 @@ public class PCController : MonoBehaviour
     void Update()
     {
         
+    }
+    public void EnterON()
+    {
+        if (this.inputField.text == "OPEN")
+        {
+         
+        }
+        else
+        {
+            this.underText.text = "パスワードが正しくありません。入力し直してください。";
+            this.okButton.SetActive(true);
+        }
+    }
+    public void OKON()
+    {
+        this.okButton.SetActive(false);
+        this.underText.text = null;
     }
 }
