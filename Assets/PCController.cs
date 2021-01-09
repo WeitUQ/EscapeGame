bb83d73a8ae9343aa1da45236b90118655bd1c3d
WeitUQ <74394582+WeitUQ@users.noreply.github.com>
2021-01-09 11:31:45 +0900

    PCログイン画面システム完成

--- Assets/PCController.cs
+++ Assets/PCController.cs
@@ -7,7 +7,9 @@ public class PCController : MonoBehaviour
 {
     public InputField inputField;
     public Text underText;
+    public GameObject inputFieldObject;
     public GameObject okButton;
+    private int errorPoint = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,13 +29,20 @@ public class PCController : MonoBehaviour
         }
         else
         {
+            this.inputFieldObject.SetActive(false);
+            this.errorPoint++;
             this.underText.text = "パスワードが正しくありません。入力し直してください。";
-            this.okButton.SetActive(true);
+            this.okButton.SetActive(true);            
         }
     }
     public void OKON()
     {
+        this.inputFieldObject.SetActive(true);
         this.okButton.SetActive(false);
         this.underText.text = null;
+        if(this.errorPoint >= 3)
+            {
+                this.underText.text = "パスワードのヒント: 閉じる = close, 開ける = ????";
+            }
     }
 }
