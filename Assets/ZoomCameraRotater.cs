421d578192b85e2bc33457fab9c0bb906dd37790
WeitUQ <74394582+WeitUQ@users.noreply.github.com>
2021-04-17 18:47:47 +0900

    文字の差し替え
    契約書、同意書、借用書の文字をunity内テキストからblnderのテキストに差し替えた。(unity内テキストではカメラのphysics.raycasterのレイヤーマスクをしていても検出されてしまうため。)

--- Assets/ZoomCameraRotater.cs
+++ Assets/ZoomCameraRotater.cs
@@ -46,144 +46,100 @@ public class ZoomCameraRotater : MonoBehaviour
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomDrawerkeys[0];
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.z);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.z);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomDrawerkeys[1].activeInHierarchy && !this.zoomDrawerkeys[0].activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomDrawerkeys[1];
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.x);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.z);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomDrawerkeys[1].activeInHierarchy && this.zoomDrawerkeys[0].activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomDrawerkeys[2];
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomCoin[0].activeInHierarchy && !this.zoomCoin[1].activeInHierarchy && !this.zoomCoin[2].activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomCoin[0];
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomCoin[1].activeInHierarchy && !this.zoomCoin[0].activeInHierarchy && !this.zoomCoin[2].activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomCoin[1];
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomCoin[2].activeInHierarchy && !this.zoomCoin[0].activeInHierarchy && !this.zoomCoin[1].activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomCoin[2];
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomCoin[0].activeInHierarchy && this.zoomCoin[1].activeInHierarchy && !this.zoomCoin[2].activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomCoin[3];
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomCoin[0].activeInHierarchy && !this.zoomCoin[1].activeInHierarchy && this.zoomCoin[2].activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomCoin[4];
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (!this.zoomCoin[0].activeInHierarchy && this.zoomCoin[1].activeInHierarchy && this.zoomCoin[2].activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomCoin[5];
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomCoin[0].activeInHierarchy && this.zoomCoin[1].activeInHierarchy && this.zoomCoin[2].activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomCoin[6];
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomHammer.activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomHammer;
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomNob.activeInHierarchy && Input.GetMouseButton(0))
             {
@@ -198,79 +154,55 @@ public class ZoomCameraRotater : MonoBehaviour
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomKey;
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomDriver.activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomDriver;
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomLight.activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = zoomLight;
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomIOU.activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomIOU;
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed,-Input.GetAxis("Mouse X") * this.rotateSpeed, 0 );
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomAgreement.activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomAgreement;
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed,  -Input.GetAxis("Mouse X") * this.rotateSpeed,0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
             else if (this.zoomContract.activeInHierarchy && Input.GetMouseButton(0))
             {
                 if (this.memorizeFrag == true)
                 {
-                    this.memorizeFrag = false;
                     this.zoomObject = this.zoomContract;
-                    this.initialRot = this.zoomObject.transform.eulerAngles;
+                    ChangeFlagRot();
                 }
-                this.rotateAngle = new Vector3(Input.GetAxis("Mouse Y") * this.rotateSpeed, -Input.GetAxis("Mouse X") * this.rotateSpeed, 0);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.up), rotateAngle.y);
-                this.zoomObject.transform.Rotate(this.zoomObject.transform.InverseTransformDirection(zoomCamera.transform.right), rotateAngle.x);
-                this.lastMousePosition = Input.mousePosition;
+                RotateObject();
             }
         }
     }
